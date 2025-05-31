06cc51d82f26abdcbd150f612d9717747565f006
Nicholas Tanugroho <117884155+Someonelse2309@users.noreply.github.com>
2025-05-31 21:35:16 +0700

    Initial Commit

---
+++ Backend.py
@@ -0,0 +1,215 @@
+import streamlit as st
+from CBR import CaseBaseReasoning
+import pandas as pd
+
+
+def startCBR(weight_range = [1.0] * 12, thresholds = 0.9):
+    caseBaseReasoning = CaseBaseReasoning(weight_range, thresholds)
+    weights = caseBaseReasoning.getWeights()
+    thresholds = caseBaseReasoning.getThresholds()
+    config = {"Gender": weights[0],
+                "Age": weights[0],
+                "Occupation": weights[0],
+                "Sleep Duration": weights[0],
+                "Quality of Sleep": weights[0],
+                "Physical Activity Level": weights[0],
+                "Stress Level": weights[0],
+                "BMI Category": weights[0],
+                "Sistole": weights[0],
+                "Diastole": weights[0],
+                "Daily Steps": weights[0],
+                "Threshold" : thresholds
+             }
+
+    return caseBaseReasoning, config
+
+caseBaseReasoning, config = startCBR()
+
+st.title("Case Base Reasoning For Sleep Disorder Detection")
+
+tab1, tab2, tab3, tab4 = st.tabs(["Program", "Comparison Data", "All Data", "Setting"])
+
+with st.sidebar:
+    st.header("Anggota Kelompok:")
+    st.write("""
+    1. Nicholas Tanugroho - 71220845
+    2. Vincen Imannuel - 71220856
+    3. Nicholas Dwinata - 71220869
+    """)
+
+with tab1:
+    st.header("Main Program")
+    genderInp = st.selectbox("Gender",
+                             tuple(sorted(("Male", "Female"))),
+                             index=None,
+                             placeholder="Enter your gender",
+                             help="The gender of the person.",
+                             )
+    ageInp = st.number_input("Age",
+                               min_value=27,
+                               max_value=59,
+                               step=1,
+                               value=None,
+                               placeholder="Enter your Age",
+                               help="The age of the person in years.",
+                               )
+    occupationInp = st.selectbox("Occupation",
+                                 tuple(sorted(("Accountant","Manager","Engineer","Scientist","Software Engineer","Teacher","Lawyer","Salesperson","Sales Representative","Doctor","Nurse"))),
+                                 index=None,
+                                 placeholder="Enter your Occupation",
+                                 help="The occupation or profession of the person.",
+                                 )
+    sleepDurationInp = st.number_input("Sleep Duration",
+                                       min_value=5.8,
+                                       max_value=8.5,
+                                       step=0.1,
+                                       value=None,
+                                       placeholder="Enter your Sleep Duration",
+                                       help="The number of hours the person sleeps per day."
+                                       )
+    qualityOfSleepInp = st.number_input("Quality of Sleep",
+                                       min_value=5.8,
+                                       max_value=8.5,
+                                       step=0.1,
+                                       value=None,
+                                       placeholder="Enter your quality of sleep score",
+                                       help="A subjective rating of the quality of sleep, ranging from 1 to 10. On the development stage the minimum is 5.8 and the maximum is 8.5.",
+                                       )
+    physicalActivityInp = st.number_input("Physical Activity Level",
+                                         min_value=30,
+                                         max_value=90,
+                                         step=1,
+                                         value=None,
+                                         placeholder="Enter your physical activity Level",
+                                         help="The number of minutes the person engages in physical activity daily. On the development stage the minimum is 30 Minute(s) and the maximum is 90 Minute(s).",
+                                         )
+    stressLevelInp = st.number_input("Stress Level",
+                                     min_value=3,
+                                     max_value=8,
+                                     step=1,
+                                     value=None,
+                                     placeholder="Enter your stress Level",
+                                     help="A subjective rating of the stress level experienced by the person, ranging from 1 to 10. On the development stage the minimum is 3 and the maximum is 8.",
+                                     )
+    BMICategoryInp = st.selectbox("BMI Category",
+                             tuple(sorted(("Normal", "Obese", "Overweight"))),
+                             index=None,
+                             placeholder="Enter your BMI Category",
+                             help="The BMI category of the person (e.g., Underweight, Normal, Overweight). On the development stage the BMI Category won't include the underweight class",
+                             )
+    sistoleInp = st.number_input("Systole",
+                                 min_value=115,
+                                 max_value=142,
+                                 step=1,
+                                 value=None,
+                                 placeholder="Enter your systole",
+                                 help="The systole of the blood pressure measurement of the person. On the development stage the minimum is 115 and the maximum is 142.",
+                                 )
+    diastoleInp = st.number_input("Diastole",
+                                 min_value=75,
+                                 max_value=95,
+                                 step=1,
+                                 value=None,
+                                 placeholder="Enter your diastole",
+                                 help="The diastole of the blood pressure measurement of the person. On the development stage the minimum is 75 and the maximum is 95.",
+                                 )
+    heartRateInp = st.number_input("Heart Rate",
+                                  min_value=65,
+                                  max_value=86,
+                                  step=1,
+                                  value=None,
+                                  placeholder="Enter your heart rate",
+                                  help="The resting heart rate of the person in beats per minute. On the development stage the minimum is 65 and the maximum is 86.",
+                                  )
+    dailyStepsInp = st.number_input("Daily Steps",
+                                   min_value=3000,
+                                   max_value=10000,
+                                   step=1,
+                                   value=None,
+                                   placeholder="Enter your daily steps",
+                                   help="The number of steps the person takes per day. On the development stage the minimum is 3000 and the maximum is 10000.",
+                                   )
+    if st.button("Submit for prediction"):
+        dictSubmit = {
+            "Gender": genderInp,
+            "Age": ageInp,
+            "Occupation": occupationInp,
+            "Sleep Duration": sleepDurationInp,
+            "Quality of Sleep": qualityOfSleepInp,
+            "Physical Activity Level": physicalActivityInp,
+            "Stress Level": stressLevelInp,
+            "BMI Category": BMICategoryInp,
+            "Sistole": sistoleInp,
+            "Diastole": diastoleInp,
+            "Heart Rate": heartRateInp,
+            "Daily Steps": dailyStepsInp
+        }
+        dfSubmit = pd.DataFrame(dictSubmit, index=[0])
+        print(dfSubmit)
+        result = caseBaseReasoning.newCase(dfSubmit,"Final")
+        st.header("Prediction Result")
+        st.write(result)
+
+
+
+
+with tab2:
+    st.header("Our Comparison Data")
+    st.dataframe(caseBaseReasoning.getDF())
+
+with tab3:
+    st.header("All Data")
+    testCSV = pd.read_csv("Sleep_health_and_lifestyle_dataset_Preproc.csv")
+    st.dataframe(testCSV)
+
+with tab4:
+    st.header("Weight Setting")
+    genderW = st.number_input(label='Gender Weight', value=config['Gender'], min_value=1.0, max_value=10.0, step=1.0)
+    ageW = st.number_input(label='Age Weight', value=config['Age'], min_value=1.0, max_value=10.0, step=1.0)
+    occupationW = st.number_input(label='Occupation Weight', value=config['Occupation'], min_value=1.0, max_value=10.0, step=1.0)
+    sleepDurationW = st.number_input(label='Sleep Duration Weight', value=config['Sleep Duration'], min_value=1.0, max_value=10.0, step=1.0)
+    qualityOfSleepW = st.number_input(label='Quality of Sleep Weight', value=config['Quality of Sleep'], min_value=1.0, max_value=10.0, step=1.0)
+    physicalActivityLevelW = st.number_input(label='Physical Activity Level', value=config['Physical Activity Level'], min_value=1.0, max_value=10.0, step=1.0)
+    stressLevelW = st.number_input(label='Stress Level', value=config['Stress Level'], min_value=1.0, max_value=10.0, step=1.0)
+    BMICategoryW = st.number_input(label='BMI Category', value=config['BMI Category'], min_value=1.0, max_value=10.0, step=1.0)
+    sistoleW = st.number_input(label='Sistole', value=config['Sistole'], min_value=1.0, max_value=10.0, step=1.0)
+    diastoleW = st.number_input(label='Diastole', value=config['Diastole'], min_value=1.0, max_value=10.0, step=1.0)
+    dailyStepsW = st.number_input(label='Daily Steps', value=config['Daily Steps'], min_value=1.0, max_value=10.0, step=1.0)
+    st.header("Threshold Setting")
+    thresholdInp = st.number_input(label='Threshold', value=config['Threshold'], min_value=0.1, max_value=1.0, step=0.05)
+
+    if st.button("Update Setting"):
+        try:
+            newWeights = [
+                float(genderW),
+                float(ageW),
+                float(occupationW),
+                float(sleepDurationW),
+                float(qualityOfSleepW),
+                float(physicalActivityLevelW),
+                float(stressLevelW),
+                float(BMICategoryW),
+                float(sistoleW),
+                float(diastoleW),
+                float(dailyStepsW)
+            ]
+            caseBaseReasoning, config = startCBR(newWeights, float(thresholdInp))
+            st.write("Config Berhasil Diupdate")
+            genderW.value = config['Gender']
+            ageW.value = config['Age']
+            occupationW.value = config['Occupation']
+            sleepDurationW.value = config['Sleep Duration']
+            qualityOfSleepW.value = config['Quality of Sleep']
+            physicalActivityLevelW.value = config['Physical Activity Level']
+            stressLevelW.value = config['Stress Level']
+            BMICategoryW.value = config['BMI Category']
+            sistoleW.value = config['Sistole']
+            diastoleW.value = config['Diastole']
+            dailyStepsW.value = config['Daily Steps']
+            thresholdInp.value = config['Threshold']
+        except ValueError as e:
+            st.error(str(e))
+            st.error("Pastikan semua input adalah angka (integer) yang valid.")
+        except KeyError as e:
+            st.error(f"Kunci konfigurasi tidak ditemukan: {e}. Harap periksa pengaturan Anda.")
+
---
+++ CBR.py
@@ -0,0 +1,305 @@
+import pandas as pd
+import json
+
+class CaseBaseReasoning():
+    def __init__(self, weight_range = [1.0] * 12, thresholds = 0.9):
+        self.occupation_mapping = {
+            "Accountant": {
+                "Accountant": 1.0,
+                "Manager": 0.6,
+                "Engineer": 0.4,
+                "Scientist": 0.3,
+                "Software Engineer": 0.3,
+                "Teacher": 0.4,
+                "Lawyer": 0.5,
+                "Salesperson": 0.3,
+                "Sales Representative": 0.3,
+                "Doctor": 0.2,
+                "Nurse": 0.2,
+            },
+            "Doctor": {
+                "Doctor": 1.0,
+                "Nurse": 0.8,
+                "Scientist": 0.6,
+                "Teacher": 0.5,
+                "Lawyer": 0.2,
+                "Engineer": 0.2,
+                "Software Engineer": 0.2,
+                "Accountant": 0.2,
+                "Salesperson": 0.1,
+                "Sales Representative": 0.1,
+                "Manager": 0.3,
+            },
+            "Engineer": {
+                "Engineer": 1.0,
+                "Software Engineer": 0.9,
+                "Scientist": 0.7,
+                "Manager": 0.6,
+                "Accountant": 0.4,
+                "Teacher": 0.4,
+                "Doctor": 0.2,
+                "Nurse": 0.2,
+                "Salesperson": 0.3,
+                "Sales Representative": 0.3,
+                "Lawyer": 0.2,
+            },
+            "Lawyer": {
+                "Lawyer": 1.0,
+                "Manager": 0.6,
+                "Accountant": 0.5,
+                "Teacher": 0.4,
+                "Salesperson": 0.3,
+                "Sales Representative": 0.3,
+                "Engineer": 0.2,
+                "Doctor": 0.2,
+                "Nurse": 0.2,
+                "Scientist": 0.3,
+                "Software Engineer": 0.3,
+            },
+            "Manager": {
+                "Manager": 1.0,
+                "Accountant": 0.6,
+                "Engineer": 0.6,
+                "Salesperson": 0.7,
+                "Sales Representative": 0.7,
+                "Lawyer": 0.6,
+                "Teacher": 0.5,
+                "Software Engineer": 0.5,
+                "Scientist": 0.5,
+                "Doctor": 0.3,
+                "Nurse": 0.3,
+            },
+            "Nurse": {
+                "Nurse": 1.0,
+                "Doctor": 0.8,
+                "Scientist": 0.5,
+                "Teacher": 0.5,
+                "Manager": 0.3,
+                "Engineer": 0.2,
+                "Software Engineer": 0.2,
+                "Accountant": 0.2,
+                "Salesperson": 0.1,
+                "Sales Representative": 0.1,
+                "Lawyer": 0.2,
+            },
+            "Sales Representative": {
+                "Sales Representative": 1.0,
+                "Salesperson": 0.95,
+                "Manager": 0.7,
+                "Teacher": 0.4,
+                "Engineer": 0.3,
+                "Software Engineer": 0.3,
+                "Lawyer": 0.3,
+                "Accountant": 0.3,
+                "Doctor": 0.1,
+                "Nurse": 0.1,
+                "Scientist": 0.2,
+            },
+            "Salesperson": {
+                "Salesperson": 1.0,
+                "Sales Representative": 0.95,
+                "Manager": 0.7,
+                "Teacher": 0.4,
+                "Engineer": 0.3,
+                "Software Engineer": 0.3,
+                "Lawyer": 0.3,
+                "Accountant": 0.3,
+                "Doctor": 0.1,
+                "Nurse": 0.1,
+                "Scientist": 0.2,
+            },
+            "Scientist": {
+                "Scientist": 1.0,
+                "Engineer": 0.7,
+                "Software Engineer": 0.7,
+                "Doctor": 0.6,
+                "Nurse": 0.5,
+                "Teacher": 0.4,
+                "Manager": 0.5,
+                "Accountant": 0.3,
+                "Lawyer": 0.3,
+                "Salesperson": 0.2,
+                "Sales Representative": 0.2,
+            },
+            "Software Engineer": {
+                "Software Engineer": 1.0,
+                "Engineer": 0.9,
+                "Scientist": 0.7,
+                "Manager": 0.5,
+                "Teacher": 0.4,
+                "Accountant": 0.3,
+                "Lawyer": 0.3,
+                "Salesperson": 0.3,
+                "Sales Representative": 0.3,
+                "Doctor": 0.2,
+                "Nurse": 0.2,
+            },
+            "Teacher": {
+                "Teacher": 1.0,
+                "Scientist": 0.4,
+                "Doctor": 0.5,
+                "Nurse": 0.5,
+                "Lawyer": 0.4,
+                "Engineer": 0.4,
+                "Software Engineer": 0.4,
+                "Accountant": 0.4,
+                "Manager": 0.5,
+                "Salesperson": 0.4,
+                "Sales Representative": 0.4,
+            },
+        }
+
+        columns = ['Gender', 'Age', 'Occupation', 'Sleep Duration', 'Quality of Sleep',
+               'Physical Activity Level', 'Stress Level', 'BMI Category',
+               'Sistole', 'Diastole', 'Daily Steps', 'Sleep Disorder']
+        self.database = pd.DataFrame(columns=columns)
+        self.index = 0
+        self.JSON_PATH = "CaseBase.json"
+        self.weight_range = weight_range
+        self.thresholds = float(thresholds)
+        self.trainData()
+
+    def trainData(self):
+        df = pd.read_csv("Sleep_health_and_lifestyle_dataset_Preproc.csv")
+        df["Sleep Disorder"] = df['Sleep Disorder'].fillna('Normal')
+
+        target_col = 'Sleep Disorder'
+        feature_cols = df.columns.drop(target_col)
+        self.features = feature_cols.tolist()
+
+        self.df_encoded = df.copy()
+        self.df_encoded['Gender'] = self.df_encoded['Gender'].map({'Male': 0, 'Female': 1})
+        self.df_encoded['BMI Category'] = self.df_encoded['BMI Category'].map({'Normal': 0, 'Overweight': 1, 'Obese': 2})
+        self.df_encoded['Sistole'] = df['Sistole'].astype(int)
+        self.df_encoded['Diastole'] = df['Diastole'].astype(int)
+
+        label_map = {'Normal': 0, 'Sleep Apnea': 1, 'Insomnia': 2, None: 0}
+        self.df_encoded['Sleep Disorder'] = self.df_encoded['Sleep Disorder'].replace('None', 'Normal')
+        self.df_encoded[target_col] = self.df_encoded[target_col].map(label_map)
+
+        for row in self.df_encoded.iterrows():
+            # print(row[1])
+            self.newCase(row[1], "Train")
+        self.saveToJson()
+
+    def newCase(self, row, State = "Train"):
+        if State == "Final":
+            row['Gender'] = row['Gender'].map({'Male': 0, 'Female': 1})
+            row['BMI Category'] = row['BMI Category'].map({'Normal': 0, 'Overweight': 1, 'Obese': 2})
+            row['Sistole'] = row['Sistole'].astype(int)
+            row['Diastole'] = row['Diastole'].astype(int)
+        # print(self.database.head())
+        if self.index == 0 and State == "Train":
+            self.retain(row)
+        else:
+            if State == "Final":
+                input_row = row.iloc[0]
+                data = self.reuse(input_row, State)
+                if data == 0:
+                    return "Normal"
+                elif data == 1:
+                    return "Sleep Apnea"
+                elif data == 2:
+                    return "Insomnia"
+                else:
+                    return "Unknown"
+            else:
+                return self.reuse(row, State)
+
+    def retain(self, row):
+        new_row = pd.DataFrame([row])
+
+        # print(new_row.head())
+        self.database = pd.concat([self.database, new_row], ignore_index=True)
+        self.index += 1
+
+
+    def reuse(self, row, state):
+        idx, sim = self.retrieve(row)
+        sim = float(sim)
+        case = self.database.iloc[idx]
+        predicted_label = case["Sleep Disorder"]
+        # print (sim, type(sim), self.thresholds, type(self.thresholds), self.thresholds <= sim)
+        if state != "Train":
+            if state == "Validation":
+                return case["Sleep Disorder"]
+            elif self.thresholds >= sim:
+                self.retain(row)
+                print(f"Most similar case found at index {idx} with similarity {sim:.4f}, Retaining Case")
+            else:
+                print(f"Most similar case found at index {idx} with similarity {sim:.4f}")
+                print("Predicted Sleep Disorder:", case["Sleep Disorder"])
+                return case["Sleep Disorder"]
+        else:
+            true_label = row["Sleep Disorder"]
+            if self.thresholds >= sim:
+                self.retain(row)
+            elif predicted_label != true_label:
+                self.retain(row)
+
+    def revise(self, row):
+        print("Revise step - currently doing nothing.")
+        self.retain(row)
+
+    def retrieve(self, row):
+        similarities = []
+        input_row = row
+        db_df = self.database
+
+        for idx, case in db_df.iterrows():
+            sim = self.calculate_similarity(input_row, case, self.weight_range)
+            similarities.append((idx, sim))
+        # print(similarities)
+        similarities.sort(key=lambda x: x[1], reverse=True)
+        # print (similarities)
+        return similarities [0]
+
+    def calculate_similarity(self, row1, row2, weights):
+        # print("Similarity ",type(row1),type(row2))
+        sim = 0
+        total_weight = sum(weights)
+        for i, col in enumerate(self.features):
+            w = weights[i-1]
+            # print (w,col,i, row1[col],type(row1[col]), row2[col],type(row2[col]))
+            if col == "Occupation":
+                s = self.occupation_mapping.get(row1[col], {}).get(row2[col], 0)
+            elif col == "Gender":
+                s = 1.0 if row1[col] == row2[col] else 0.0
+            elif col == "BMI Category":
+                s = 1.0 - abs(row1[col] - row2[col]) / 2.0
+            else:
+                try:
+                    max_val = self.df_encoded[col].max()
+                    s = 1.0 - abs(row1[col] - row2[col]) / max_val
+                except Exception as e:
+                    print  (e)
+                    print(col)
+                    s = 0
+            sim += w * s
+            # print(s)
+        # print (self.thresholds, sim, total_weight, weights)
+        return sim / total_weight if total_weight != 0 else 0
+
+    def saveToJson(self):
+        df = self.database
+        data_dict = df.to_dict(orient="index")
+        dictFinal = dict()
+        dictFinal["Weights"] = self.weight_range
+        dictFinal["Thresholds"] = self.thresholds
+        dictFinal["Cases"] = data_dict
+        with open(self.JSON_PATH, "w") as f:
+            json.dump(dictFinal, f, indent=4)
+
+    def getWeights(self):
+        return self.weight_range
+
+    def getThresholds(self):
+        return self.thresholds
+
+    def getDF(self):
+        return self.database
+
+
+if __name__ == "__main__":
+    cbr = CaseBaseReasoning()
+    print (cbr.getWeights())
